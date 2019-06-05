1fed38549280667b47247f457240294cb3ca6ce7
erib <eri-b@users.noreply.github.com>
2019-06-05 17:34:20 -0400

    renamed and added

---
+++ select_name.sql
@@ -0,0 +1,54 @@
+-- 1
+SELECT name FROM world
+  WHERE name LIKE 'Y%'
+
+-- 2
+SELECT name FROM world
+  WHERE name LIKE '%Y'
+
+-- 3
+SELECT name FROM world
+  WHERE name LIKE '%x%'
+
+-- 4
+SELECT name FROM world
+  WHERE name LIKE '%land'
+
+-- 5
+SELECT name FROM world
+  WHERE name LIKE 'c%ia'
+
+-- 6
+SELECT name FROM world
+  WHERE name LIKE '%oo%'
+
+-- 7
+SELECT name FROM world
+  WHERE name LIKE '%a%a%a%'
+
+-- 8
+SELECT name FROM world
+ WHERE name LIKE '_t%'
+ORDER BY name
+
+-- 9
+SELECT name FROM world
+ WHERE name LIKE '%o__o%'
+
+-- 10
+SELECT name FROM world
+ WHERE name LIKE '____'
+
+-- 11
+
+
+-- 12
+
+
+-- 13
+
+
+-- 14
+
+
+-- 15
