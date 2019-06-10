9911c5b067ef4bbce5aa6c1d6ae53059eb029f9e
erib <eri-b@users.noreply.github.com>
2019-06-10 15:38:41 -0400

    self join

--- self_join.sql
+++ self_join.sql
@@ -40,3 +40,19 @@ FROM route a JOIN route b ON
   JOIN stops stopb ON (b.stop=stopb.id)
 WHERE stopa.name='Craiglockhart'
 AND stopb.name = 'London Road'
+
+--7
+SELECT DISTINCT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+WHERE a.stop=115
+AND b.stop= 137
+
+--8
+SELECT a.company, a.num
+FROM route a JOIN route b ON
+  (a.company=b.company AND a.num=b.num)
+  JOIN stops stopa ON (a.stop=stopa.id)
+  JOIN stops stopb ON (b.stop=stopb.id)
+WHERE stopa.name='Craiglockhart'
+AND stopb.name = 'Tollcross'
